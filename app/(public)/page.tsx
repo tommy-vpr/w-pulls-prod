c2648aa326808cc3e0e8192e581143e3f5361f13
tommy-vpr <tommy@cultivatedagency.com>
2026-06-19 15:19:16 -0700

    Add script for klaviyo onsite tracking - klaviyo form

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,18 +1,29 @@
 import Image from "next/image";
-import React from "react";
+import Script from "next/script";
 
-const page = () => {
+export default function Page() {
   return (
-    <div className="h-screen flex justify-center flex-col items-center gap-4">
-      <Image
-        src="/images/w-pull-logo.png"
-        width={100}
-        height={100}
-        alt="WPulls"
+    <>
+      <Script
+        id="klaviyo-onsite"
+        strategy="afterInteractive"
+        src="https://static.klaviyo.com/onsite/js/TqGyCz/klaviyo.js?company_id=TqGyCz"
       />
-      <h3 className="text-3xl text-gray-200 font-semibold">WPulls</h3>
-    </div>
-  );
-};
 
-export default page;
+      <div className="h-screen flex justify-center flex-col items-center gap-4 px-6">
+        <Image
+          src="/images/w-pull-logo.png"
+          width={100}
+          height={100}
+          alt="W-Pulls"
+        />
+
+        <h3 className="text-3xl text-cyan-500 font-semibold tracking-wide">
+          WPulls
+        </h3>
+
+        <div className="klaviyo-form-X2JQrP"></div>
+      </div>
+    </>
+  );
+}
